5314b9c23f47487b6bf6cbf941f7f5043994a976
thraddash <theartist@gmail.com>
2021-04-21 14:28:14 -0700

    convert onInputChange and onFormSubmit with proper syntax with const in function component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,42 +1,32 @@
 import React, { useState } from 'react';
 
-const SearchBar = () => {
+const SearchBar = ({ onFormSubmit }) => {
     const [term, setTerm] = useState('');
-};
 
-class SearchBar extends React.Component {
-
-    onInputChange = (event) => {
-        this.setState({ term: event.target.value });
+    const onInputChange = (event) => {
+        setTerm(event.target.value);
     };
-
-    onFormSubmit = event => {
+    
+    const onSubmit = event => {
         event.preventDefault();
 
-        // TODO: Make sure we call
-        // callback from parent component this.prop.onFormSubmit 
-        // is told what the current search term is this.state.term
-        this.props.onFormSubmit(this.state.term)
+        onFormSubmit(term);
     };
 
-    // alternative: pass onInputChange() inline to onChange event handler
-    // onChange={e => this.setState({ term: e.target.value })}
-    render() {
-        return (
-            <div className="search-bar ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                    <div className="field">
-                        <label>Video Search</label>
-                        <input 
-                            type="text" 
-                            value={this.state.term} 
-                            onChange={this.onInputChange}
-                        />
-                    </div>
-                </form>
-            </div>
-        );
-    }
-}
 
+    return (
+        <div className="search-bar ui segment">
+            <form onSubmit={onSubmit} className="ui form">
+                <div className="field">
+                    <label>Video Search</label>
+                    <input 
+                        type="text" 
+                        value={term} 
+                        onChange={onInputChange}
+                    />
+                </div>
+            </form>
+        </div>
+    );
+};
 export default SearchBar;
