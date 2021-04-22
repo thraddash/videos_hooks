6ee3acb15f6fd1cdc13868918cfee63bb3c1d8bf
thraddash <theartist@gmail.com>
2021-04-21 22:09:39 -0700

    clean up App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import SearchBar from './SearchBar';
-import youtube from '../apis/youtube';
 import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
+import useVideos from '../hooks/useVideos';
 
 const App = () => {
-    const [videos, setVideos] = useState([]);
+    
     const [selectedVideo, setSelectedVideo] = useState(null);
+    const [videos, search] = useVideos('forest');
 
     useEffect(() => {
-        onTermSubmit('buildings');
-    }, []); // 2nd argv empty array, run inside function one time
+        setSelectedVideo(videos[0]);
+    }, [videos]);
 
-    const onTermSubmit = async term => {
-        const response = await youtube.get('/search', {
-            params: {
-                q: term
-            }
-        });
-
-        setVideos(response.data.items);
-        setSelectedVideo(response.data.items[0]);
-    };
 
     return (
         <div className="ui container">
-            <SearchBar onFormSubmit={onTermSubmit} />
+            <SearchBar onFormSubmit={search} />
             <div className="ui grid">
                 <div className="ui row">
                     <div className="eleven wide column">
